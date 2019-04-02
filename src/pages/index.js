ff4cd21373b271f6d8aa48088557349044e571a2
OgheneMichael <zonalmeet@gmail.com>
2019-04-02 03:09:17 +0100

    Stacked components to create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
-import React from "react"
+import React, { Fragment } from "react";
+import Alumni from "../components/alumni";
+import ApplicationProcess from "../components/application-process";
+import FAQ from "../components/faq";
+import FlashBar from "../components/flash-bar";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import HiringPartners from "../components/hiring-partners";
+import HomeHero from "../components/home-hero";
+import Investors from "../components/investors";
+import Mentors from "../components/mentors";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Fragment>
+    <FlashBar />
+    <div id="content" className="container-fluid pub-content">
+      <div className="row">
+        <Header />
+        <HomeHero />
+        <ApplicationProcess />
+        <HiringPartners />
+        <Mentors />
+        <FAQ />
+        <Alumni />
+        <br />
+        <br />
+        <Investors />
+      </div>
+    </div>
+    <Footer />
+  </Fragment>
+);
