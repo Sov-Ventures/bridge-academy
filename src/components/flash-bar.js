6466aff81c05465f6f5edccdb3e8fd93ca88593e
OgheneMichael <zonalmeet@gmail.com>
2019-04-02 03:08:46 +0100

    Transformed flash bar to a component

---
+++ src/components/flash-bar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function FlashBar() {
+  return (
+    <center id="flash-bar">
+      <span role="img" aria-label="icon">
+        🏆
+      </span>{" "}
+      The April Tournament is live!{" "}
+      <a
+        style={{ fontWeight: "bold", color: "white" }}
+        href="/tournament"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Join the tournament&#187;
+      </a>
+    </center>
+  );
+}
