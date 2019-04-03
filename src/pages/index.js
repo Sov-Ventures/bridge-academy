83b8868a6876cd644af2ff8c8fb9a1c8f35fcedd
OgheneMichael <zonalmeet@gmail.com>
2019-04-03 05:59:56 +0100

    Used layout component in the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,25 +8,28 @@ import Header from "../components/header";
 import HiringPartners from "../components/hiring-partners";
 import HomeHero from "../components/home-hero";
 import Investors from "../components/investors";
+import Layout from "../components/layout";
 import Mentors from "../components/mentors";
 
 export default () => (
   <Fragment>
     <FlashBar />
-    <div id="content" className="container-fluid pub-content">
-      <div className="row">
-        <Header />
-        <HomeHero />
-        <ApplicationProcess />
-        <HiringPartners />
-        <Mentors />
-        <FAQ />
-        <Alumni />
-        <br />
-        <br />
-        <Investors />
-      </div>
-    </div>
+    <Layout>
+      <Header />
+      <HomeHero />
+      <ApplicationProcess />
+      <HiringPartners />
+      <Mentors
+        desc={`Our team of mentors host office hours, review your final project ideas
+          and give you detailed and personalized feedback throughout the
+          program.`}
+      />
+      <FAQ />
+      <Alumni />
+      <br />
+      <br />
+      <Investors />
+    </Layout>
     <Footer />
   </Fragment>
 );
