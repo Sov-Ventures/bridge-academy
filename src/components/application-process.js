3267790315e7e715d9704e9e42da5ae603be66ca
OgheneMichael <zonalmeet@gmail.com>
2019-04-03 07:22:53 +0100

    Turned Application Process to a reusable component

--- src/components/application-process.js
+++ src/components/application-process.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ApplicationProcess() {
+export default function ApplicationProcess({ headline, desc, data }) {
   return (
     <section id="apply" className="tools">
       <div className="triangle triangle--bottom triangle--gray">
@@ -20,51 +20,25 @@ export default function ApplicationProcess() {
         <div>
           <h2 className="section-header">
             <big>
-              <span style={{ textAlign: "center" }}>Application Process</span>
+              <span style={{ textAlign: "center" }}>{headline}</span>
             </big>
           </h2>
           <div className="container" id="how-items">
             <p className="description" align="center">
-              Our three step process to joining.
+              {desc}
             </p>
 
-            <div id="how-item-1" className="how-item">
-              <h3>
-                <b>Online Application</b>
-              </h3>
-              <div className="progress">
-                <div className="progress-bar" style={{ width: "33%" }} />
+            {data.map(how => (
+              <div key={how.headline} className="how-item">
+                <h3>
+                  <b>{how.headline}</b>
+                </h3>
+                <div className="progress">
+                  <div className="progress-bar" style={{ width: "33%" }} />
+                </div>
+                <p>{how.desc}</p>
               </div>
-              <p>
-                Complete a 10-15 minute application covering your background,
-                goals, and experience.{" "}
-              </p>
-            </div>
-            <div id="how-item-2" className="how-item">
-              <h3>
-                <b>Fit Interview</b>
-              </h3>
-              <div className="progress">
-                <div className="progress-bar" style={{ width: "66%" }} />
-              </div>
-              <p>
-                A short IM interview to assess your fit for our program, after
-                which we'll send you a customized program plan based on your
-                goals.
-              </p>
-            </div>
-            <div id="how-item-3" className="how-item">
-              <h3>
-                <b>Accept &amp; Start</b>
-              </h3>
-              <div className="progress">
-                <div className="progress-bar" style={{ width: "100%" }} />
-              </div>
-              <p>
-                If accepted, we'll send you an offer. Select a payment plan, or
-                choose from one of our many financing options.
-              </p>
-            </div>
+            ))}
             <br style={{ clear: "both" }} />
             <br />
           </div>
