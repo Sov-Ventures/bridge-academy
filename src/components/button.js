846d4bc6512aec3cdfaeb9d325b10707ec7fd6d2
OgheneMichael <zonalmeet@gmail.com>
2019-04-04 01:45:19 +0100

    refactored button component

--- src/components/button.js
+++ src/components/button.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { Link } from "gatsby";
 
 export default function Buttom({ children, to }) {
   return (
     <p>
-      <a
-        style={{ display: "inline-block" }}
-        className="btn btn-primary"
-        href={to}
-      >
-        {children}
-      </a>
+      {/^\/(?!\/)/.test(to) ? (
+        <Link to={to} className="btn btn-primary">
+          {children}
+        </Link>
+      ) : (
+        <a
+          style={{ display: "inline-block" }}
+          className="btn btn-primary"
+          href={to}
+        >
+          {children}
+        </a>
+      )}
     </p>
   );
 }
