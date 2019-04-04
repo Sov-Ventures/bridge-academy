7c8d91d50a7c318d8498b96620e3c5b3a3264adf
OgheneMichael <zonalmeet@gmail.com>
2019-04-04 01:45:50 +0100

    added subHeadline as a prop in Hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,30 +1,41 @@
 import React from "react";
 
-export default function Hero({ children, headline, firstDesc, secondDesc }) {
+export default function Hero({
+  children,
+  headline,
+  subHeadline,
+  firstDesc,
+  secondDesc,
+  className,
+}) {
   return (
     <section className="hero">
-      {headline && (
-        <h1>
-          <span style={{ textAlign: "center" }}>
-            {headline}
-            <big style={{ color: "#59cbb9" }}>.</big>
-          </span>
-        </h1>
-      )}
+      <div className={className}>
+        {headline && (
+          <h1>
+            <span style={{ textAlign: "center" }}>
+              {headline}
+              <big style={{ color: "#59cbb9" }}>.</big>
+            </span>
+          </h1>
+        )}
 
-      {firstDesc && (
-        <p className="description" align="center">
-          {firstDesc}
-        </p>
-      )}
+        {subHeadline && <h3>{subHeadline}</h3>}
 
-      {secondDesc && (
-        <p className="description" align="center">
-          {secondDesc}
-        </p>
-      )}
+        {firstDesc && (
+          <p className="description" align="center">
+            {firstDesc}
+          </p>
+        )}
 
-      {children}
+        {secondDesc && (
+          <p className="description" align="center">
+            {secondDesc}
+          </p>
+        )}
+
+        {children}
+      </div>
     </section>
   );
 }
