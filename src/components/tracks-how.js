be0f185fe3898c8efc65cc09e6d991b44c81e789
OgheneMichael <zonalmeet@gmail.com>
2019-04-04 01:48:54 +0100

    Created how component for career tracks

---
+++ src/components/tracks-how.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+export default function TracksHow() {
+  return (
+    <section id="how-items" className="container">
+      <div id="faq-items">
+        <div className="faq-item">
+          <h5>LOCATION</h5>
+          <p>
+            Online - anywhere in the world. The majority of the program can be
+            taken asynchonously with office hours being one of the main
+            synchronous components.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h5>PAYMENT OPTIONS</h5>
+          <p>
+            1. $0 up-front + $19,000 maximum repayment via an ISA agreement,
+            repaid while employed. <a href="#faq">See FAQ</a> for details.
+          </p>
+          <p>2. $13,000 up-front + no income-based repayment.</p>
+        </div>
+
+        <div className="faq-item">
+          <h5>Optional: Tournament Phase (4 weeks)</h5>
+          <p>
+            In order to participate in one of the Bridge programs, you first
+            need to either a) demonstrate a basic competence in the required
+            track skills through a previous project OR b) join the monthly
+            tournament.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h5>Phase 1 (4-8 weeks): Project &amp; Mentoring</h5>
+          <p>
+            For these phase you'll be matched with a personal mentor who will
+            provide you feedback on your project. The project will map to the
+            new skills you've acquired and would like to take to the next level.{" "}
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h5>Phase 2 (4-8 weeks): Interview Prep &amp; Interviews</h5>
+          <p>
+            During this phase you'll run practice interviews and challenges and
+            get feedback. You'll also be connected with companies to interview
+            with based on your target job title. You may repeat this phase as
+            much as you need to before receiving offers.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h5>Optional: Onboarding Bootcamp</h5>
+          <p>
+            Some employers may opt to have you enter a special on-boarding
+            bootcamp where you will learn the specifics of your new employer's
+            tech stack from top to bottom so you can hit the ground running.
+            This phase is optional and is required by some employers and not
+            others.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h5>Optional: Post-Grad Apprenticeship</h5>
+          <p>
+            We also offer an optional 3-9 month long paid apprenticeship program
+            through one of our network of hiring partners. Apprenticeships pay
+            cost-of-living stipends and can be a good bridge into your new
+            career.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h5>PROGRAM BENEFITS</h5>
+
+          <ul>
+            <li>- 1:1 Mentorship from leading industry experts</li>
+            <li>- Private TA + Discussion Forum Access</li>
+            <li>- Custom Course Curriculum &amp; Excercises</li>
+            <li>- Resume Review + Professional Headshots</li>
+            <li>- Access to Alumni network job alerts</li>
+            <li>- Continued Career Services Post-Program</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
