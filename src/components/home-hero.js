64294d42e7607c06db15776166899ab4157e8890
Adam Breckler <abreckler@gmail.com>
2019-04-17 19:35:38 -0700

    updates

--- src/components/home-hero.js
+++ src/components/home-hero.js
@@ -38,7 +38,7 @@ class HomeHero extends Component {
             </h1>
 
             <p style={{ marginBottom: "0" }}>
-              Bridge is a globally distributed training &amp; talent network for frontier technology.
+              Bridge is a globally distributed training &amp; tech talent network.
             </p>
 
             <p style={{ marginBottom: "0" }}>
